93dcca36d5cd61cf9d0af1a2cee83bc9f4ee4e37
Ferroxzim <142860285+Ferroxzim@users.noreply.github.com>
2026-03-26 08:29:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,5 +103,8 @@ def delete_usuario(id):
 
     return redirect('/')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
